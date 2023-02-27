3a7a898b856250c928d1d46c31d115d57008340d
mdshahnawaz786 <mdshahnawaz911@gmail.com>
2023-02-28 03:36:28 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+import ReactMarkdown from 'react-markdown'
 
 function App() {
+  const [val, setVal] = useState()
+
   return (
     <div className="App">
-      hiiiiiiiiii
+
+        <div className='firstDiv'>
+          <h1>Markdown Input</h1>
+            <div className='divdiv'>
+            <input type="text" value={val} placeholder="write markdown syntax" onChange={(event)=>{
+          setVal(event.target.value)
+        }}></input>
+            </div>
+        </div>
+        <div className='secDiv'>
+        <h1>Markdown Preview</h1>
+        <div className='divdiv'>
+          
+        <ReactMarkdown>{val}</ReactMarkdown>
+        </div>
+        </div>
+
     </div>
   );
 }
